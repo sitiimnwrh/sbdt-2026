a8fc2adcd0362fb567b43c483e9fb90ae9b03829
root <root@LAPTOP-GOL54139.localdomain>
2026-05-23 19:26:52 +0700

    sbdt

---
+++ src/database/seeders/StudentSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Student;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Student::firstOrCreate([
+            'nim' => '20230801457',
+            'name' => 'Siti'
+        ]);
+    }
+}
